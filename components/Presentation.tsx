be90163b16ca858f997977ad4ffb143214701941
Alejandro Villanueva <alejandro.villanuevaf@hotmail.com>
2024-04-26 11:07:13 -0500

    Update URL of my CV

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -5,7 +5,7 @@ export const Presentation = () => {
     <section id="home" className='flex flex-col items-center justify-center h-full py-24'>
         <h1 className="flex gap-4 items-center text-2xl md:text-4xl lg:text-6xl my-20 lg:my-24 animate-fadeIn">Hola, soy Alejandro Villanueva <span className="animate-waving-hand">👋</span></h1>
         <p className='m-auto mb-10 lg:text-2xl animate-fadeIn text-brightTurquoise'>FullStack Developer</p>
-        <a href="https://drive.google.com/file/d/1VNhdLJWWEcHctNCrczk_2nl5cAEds7wA/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="max-w-48 mb-10 bg-brightTurquoise bg opacity-90 hover:bg-opacity-80 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 me-2">
+        <a href="https://drive.google.com/file/d/1etmBtikbxmCcJSH9zKTtDnhByBw1KGOy/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="max-w-48 mb-10 bg-brightTurquoise bg opacity-90 hover:bg-opacity-80 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 me-2">
           Ver CV
         </a>
         <ul className='flex gap-5 justify-center mb-10'>
